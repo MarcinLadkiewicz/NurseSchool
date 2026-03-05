05d116fa1edf4829c1565a2be043588ef062e6e8
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-05 11:54:31 +0100

    chore: created studensRoutes

---
+++ backend/routes/studentsRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const auth = require('../middlewares/authMiddleware');
+const role = require('../middlewares/roleMiddleware');
+const ctrl = require('../controllers/studentsController');
+
+router.get('/', auth, role(['enfermero']), ctrl.getAll);
+router.get('/:id', auth, role(['enfermero']), ctrl.getById);
+router.post('/', auth, role(['enfermero']), ctrl.create);
+router.put('/:id', auth, role(['enfermero']), ctrl.update);
+
+router.get('/padre/:padreId', auth, role(['padre']), ctrl.getByFatherId);
+
+module.exports = router;
