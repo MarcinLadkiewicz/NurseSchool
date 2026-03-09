a7b288a48d011c49ed50751a770f9b2905fbe77c
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-09 14:27:39 +0100

    chore: defining attention routes

---
+++ backend/routes/attentionRoutes.js
@@ -0,0 +1,26 @@
+const router = require ('express').Router();
+const ctrl = require('../controllers/attentionController');
+const auth = require('../middlewares/authMiddleware');
+const role = require('../middlewares/roleMiddleware');
+
+
+router.get('/', auth, role(['enfermero']), ctrl.getAllAttentions);
+router.get('/:id',  auth, role(['enfermero']), ctrl.getById);
+router.get('/alumno/:id', auth, role(['enfermero']), ctrl.getByStudentId);
+
+router.post('/', auth, role(['enfermero']), ctrl.createAttention);
+
+
+/*
+
+AÑADIR MIDDLEWARE DE MULTER PARA HACER UPLOADS DE DOCUMENTOS. 
+
+router.post('/:id', auth, role(['enfermero']), ctrl.postAttentionDocument);
+
+
+
+
+
+
+*/
+module.exports = router;
