8bd7aa9caba3ded5cef3e82d7482b21dd4517fd7
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-12 15:54:15 +0100

    chore: added NurseTabs

---
+++ frontend/src/navigation/Tabs/NurseTabs.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import NurseHomeScreen from '../../screens/nurse/NurseHomeScreen';
+import StudentsScreen from '../../screens/nurse/StudentsScreen'; 
+import NewAttentionScreen from '../../screens/nurse/NewAttentionScreen';
+import AllergiesScreen from '../../screens/nurse/AllergiesScreen';
+
+
+
