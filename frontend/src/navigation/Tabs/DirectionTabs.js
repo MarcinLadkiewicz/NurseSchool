c188e1067830776a9b71afa041e95677e990061f
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-15 16:14:39 +0100

    chore: creation of DirectionTabs

---
+++ frontend/src/navigation/Tabs/DirectionTabs.js
@@ -0,0 +1,38 @@
+import react from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import DirectionHomeScreen from '../../screens/direction/DirectionHomeScreen';
+import SettingsScreen from '../../screens/shared/SettingsScreen';
+import { darkTheme as colors } from '../../theme/colors';
+import { Ionicons } from '@expo/vector-icons';
+
+
+const Tab = createBottomTabNavigator();
+
+const DirectionTab = () => {
+    return(
+        <Tab.Navigator screenOption={{headerShown : false}}>    
+            <Tab.Screen name = 'DirectionHome' component={DirectionHomeScreen} options={{
+                tabBarIcon : (focused, size) => (
+                    <Ionicons
+                        name={focused ? 'home' : 'home-outline'}
+                        size={size || 20}
+                        color={colors.primary}
+                    />
+                )
+            }}/>
+            <Tab.Screen name = 'Settings' component={SettingsScreen} options={{
+                tabBarIcon : (focused, size) => (
+                    <Ionicons
+                        name = {focused ? 'settings' : 'settings-outline'}
+                        size = {size || 20}
+                        color = {colors.primary}
+                    />
+                )
+            }}/>
+        </Tab.Navigator>
+    )
+}
+
+
+export default DirectionTab;
