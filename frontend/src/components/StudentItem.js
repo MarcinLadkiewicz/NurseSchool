edbedd4ed76e916bb530eb07930f95a076a1a471
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-16 17:31:28 +0100

    feat: changed warning UI

--- frontend/src/components/StudentItem.js
+++ frontend/src/components/StudentItem.js
@@ -2,27 +2,31 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import {darkTheme as colors} from '../theme/colors';
 
-const StudentItem = ({item, navigation}) => 
-    (<TouchableOpacity
-      style={styles.studentItem}
-      onPress={() =>
-        navigation.navigate("Detail", { id: item.id })
-      }
-    >
-      {/*Icono Avatar*/}
-      <View style={styles.avatar}>
-        <Ionicons name="person-outline" size={22} color={colors.primary} />
-      </View>
-      {/*Info*/}
-      <View style={styles.studentInfo}>
-        <Text style={styles.studentName}>
-          {item.name} {item.surname}
-        </Text>
-      </View>
-      <View>
-        <Text style={styles.studentMore}>{item.course} </Text>
-      </View>
-    </TouchableOpacity>);
+const StudentItem = ({ item, navigation }) => (
+  <TouchableOpacity
+    style={styles.studentItem}
+    onPress={() => navigation.navigate("Detail", { id: item.id })}
+  >
+    {/*Icono Avatar*/}
+    <View style={styles.avatar}>
+      <Ionicons name="person-outline" size={22} color={colors.primary} />
+    </View>
+    {/*Info*/}
+    <View style={styles.studentInfo}>
+      <Text style={styles.studentName}>
+        {item.name} {item.surname}
+      </Text>
+    </View>
+    <View style={styles.studentRight}>
+      <Text style={styles.studentMore}>{item.course} </Text>
+      {item.allergy_count > 0 && (
+        <View style={styles.badge}>
+          <Ionicons name='warning' size={14} color={colors.danger}/>
+        </View>
+      )}
+    </View>
+  </TouchableOpacity>
+);
 
 
 
@@ -48,7 +52,30 @@ const styles = StyleSheet.create({
   },
   studentInfo: { flex: 1 },
   studentName: { fontSize: 15, fontWeight: "600", color: colors.textPrimary },
-  studentMore: { fontSize: 13, color: colors.textSecondary, marginTop: 2 },
+  studentMore: {
+    flexDirection: "row",
+    fontSize: 13,
+    color: colors.textSecondary,
+    marginTop: 2,
+  },
+  studentRight: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  badge: {
+    backgroundColor: colors.dangerBg,
+    borderRadius: 12,
+    width: 24,
+    height: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  badgeText: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors.danger,
+  },
 });
 
 export default StudentItem;
