c255bfce08d2c168334b9405206298dff703cc03
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-16 15:02:52 +0100

    feat: Student screen and detail

---
+++ frontend/src/screens/nurse/StudentDetailScreen.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { AuthContext } from '../../context/AuthContext';
+
+const StudentDetailScreen = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Panel Atenciones</Text>
+      <Text style={styles.sub}>Bienvenido, {user?.nombre}</Text>
+      <TouchableOpacity style={styles.boton} onPress={logout}>
+        <Text style={styles.botonTexto}>Cerrar sesión</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F7FA",
+  },
+  title: { fontSize: 24, fontWeight: "bold", color: "#0D2B4E" },
+  sub: { fontSize: 16, color: "#5A7A9A", marginTop: 8 },
+  boton: {
+    backgroundColor: "#E74C3C",
+    borderRadius: 10,
+    padding: 14,
+    marginTop: 30,
+  },
+  botonTexto: { color: "#fff", fontWeight: "bold" },
+});
+
+export default StudentDetailScreen;
--- frontend/src/screens/nurse/StudentsScreen.js
+++ frontend/src/screens/nurse/StudentsScreen.js
@@ -1,37 +1,127 @@
-import React, { useContext } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList, RefreshControl, ActivityIndicator } from 'react-native';
 import { AuthContext } from '../../context/AuthContext';
+import {darkTheme as colors} from '../../theme/colors';
+import StudentItem from '../../components/StudentItem';
+import api from '../../api/axios';
+import { Ionicons } from '@expo/vector-icons';
 
-const StudentsScreen = () => {
-  const { user, logout } = useContext(AuthContext);
+const StudentsScreen = ({navigation}) => {
+  const {user} = useContext(AuthContext);
+
+  const [data, setData] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadStudents = async () => {
+    try{
+      const res = await api.get('/students');
+      setData(res.data);
+    } catch (err) {
+      console.log('Error cargando los alumnos', err.message)
+    } finally {
+      setLoading(false)
+      setRefreshing(false)
+    }
+  }
+  const filtered = data.filter((item) =>
+    `${item.name} ${item.surname}`
+      .toLowerCase()
+      .includes(inputText.toLowerCase()),
+  );
+
+  useEffect(()=>{
+    loadStudents();
+  }, []);
+
+  const onRefresh = useCallback(()=>{
+    setRefreshing(true);
+    loadStudents();
+  }, []);
+
+  if (loading) {
+       return (
+         <View style={[styles.container, styles.center]}>
+           <ActivityIndicator size="large" color={colors.primary} />
+         </View>
+       );
+     }
+
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Panel Alumnos</Text>
-      <Text style={styles.sub}>Bienvenido, {user?.nombre}</Text>
-      <TouchableOpacity style={styles.boton} onPress={logout}>
-        <Text style={styles.botonTexto}>Cerrar sesión</Text>
+      <TouchableOpacity style={styles.addStudent} onPress={()=> navigation.navigate('AddStudent')}>
+        <Ionicons name='add' size={20} color={colors.primary}/>
       </TouchableOpacity>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='Buscar alumno...' placeholderTextColor={colors.textMuted} onChangeText={setInputText} value={inputText}/>
+      </View>
+      <FlatList
+        data={filtered}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => <StudentItem item={item} navigation={navigation}/>}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            tintColor={colors.primary}
+          />
+        }
+        />
     </View>
+    
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.background,
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  center: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5F7FA",
   },
-  title: { fontSize: 24, fontWeight: "bold", color: "#0D2B4E" },
-  sub: { fontSize: 16, color: "#5A7A9A", marginTop: 8 },
-  boton: {
-    backgroundColor: "#E74C3C",
-    borderRadius: 10,
-    padding: 14,
-    marginTop: 30,
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: colors.textPrimary,
+    marginBottom: 16,
   },
-  botonTexto: { color: "#fff", fontWeight: "bold" },
+  addStudent: {
+    position: "absolute",
+    top: 60,
+    right: 20,
+    width: 44,
+    height: 44,
+    borderRadius: 14,
+    backgroundColor: colors.accentSoft,
+    borderWidth: 1,
+    borderColor: colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: colors.inputBg,
+    borderRadius: 14,
+    borderWidth: 1,
+    borderColor: colors.border,
+    paddingHorizontal: 14,
+    marginBottom: 16,
+    marginTop: 30
+  },
+  input: {
+    flex: 1,
+    paddingVertical: 14,
+    fontSize: 15,
+    color: colors.textPrimary,
+  },
 });
 
 export default StudentsScreen;
