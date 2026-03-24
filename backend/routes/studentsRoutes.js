58bf4e8e50931c3b883c5a3acd6c324fcc3c20f8
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-03-24 14:24:50 +0100

    feat: added parent permission on endpoint

--- backend/routes/studentsRoutes.js
+++ backend/routes/studentsRoutes.js
@@ -5,7 +5,7 @@ const ctrl = require('../controllers/studentsController');
 
 router.get('/', auth, role(['enfermero']), ctrl.getAll);
 router.get('/parents/list', auth, role(['enfermero']), ctrl.getParents);
-router.get('/:id', auth, role(['enfermero']), ctrl.getById);
+router.get('/:id', auth, role(['enfermero', 'padre']), ctrl.getById);
 router.post('/', auth, role(['enfermero']), ctrl.create);
 router.put('/:id', auth, role(['enfermero']), ctrl.update);
 
