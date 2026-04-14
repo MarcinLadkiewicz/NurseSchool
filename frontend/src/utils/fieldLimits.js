2f4a5459e7f6be575feaba003a0e36bec1ecae2e
Marcin Ladkiewicz <marcin.ladkiewicz@gmail.com>
2026-04-14 11:16:01 +0200

    chore: created util for textInput validation on frontend

---
+++ frontend/src/utils/fieldLimits.js
@@ -0,0 +1,14 @@
+export const FIELD_LIMITS = 
+    {
+    userName: 100, 
+    userEmail: 150,
+    studentName: 50,
+    studentSurname: 100,
+    studentCourse: 20,
+    attentionReason: 255, 
+    attentionActuation: 500,
+    attentionDescription: 1000,
+    allergyDescription: 255,
+    pathologyName: 100,
+    pathologyDescription: 1000,
+    }
